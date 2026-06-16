3eb333262f70db3d6cb913605701fb6a51221cc0
Naviganet <dadegi@gmail.com>
2026-06-16 12:57:55 +0200

    Codici lezione W7D2

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,4 +26,15 @@ console.log(newPerson);
 console.log(JSON.stringify(newPerson));
 
 let myString = JSON.stringify(newPerson);
-console.log(JSON.parse(myString));
+console.table(JSON.parse(myString));
+
+localStorage.setItem('persons', newPerson); // Sbagliato, local/sessionStorage possono memorizzare solo stringhe
+localStorage.setItem('persons', JSON.stringify(newPerson)); // Corretto
+
+const myArray = [1, 2, 3, 4, 'Pippo', 'Pluto', 'Paperino'];
+localStorage.setItem('array', myArray); // Sbagliato, trasforma l'array in una stringa generica
+localStorage.setItem('array', JSON.stringify(myArray)); //Corretto
+
+let arrayFromLS = localStorage.getItem('array'); // Sbagliato, legge soltanto una stringa
+console.log(arrayFromLS);
+console.log(JSON.parse(localStorage.getItem('array'))); // Corretto
